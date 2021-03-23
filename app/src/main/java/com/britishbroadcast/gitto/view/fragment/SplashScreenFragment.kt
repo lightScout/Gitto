d36e7830abb6083206bdef79a98b692938615bc1
MindaRah <minder_r@hotmail.co.uk>
2021-03-23 14:21:38 +0000

    .update LoginScreenFragment .update login_fragment_layout.xml

--- app/src/main/java/com/britishbroadcast/gitto/view/fragment/SplashScreenFragment.kt
+++ app/src/main/java/com/britishbroadcast/gitto/view/fragment/SplashScreenFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.britishbroadcast.gitto.R
 import com.britishbroadcast.gitto.databinding.SplashScreenLayoutBinding
+import com.britishbroadcast.gitto.view.ui.fragment.LoginScreenFragment
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_fragment_layout.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -15,8 +20,13 @@ import java.util.*
 class SplashScreenFragment: Fragment() {
 
     private lateinit var binding: SplashScreenLayoutBinding
+    private val loginScreenFragment = LoginScreenFragment()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = SplashScreenLayoutBinding.inflate(inflater, container, false)
 
         return binding.root
@@ -29,16 +39,30 @@ class SplashScreenFragment: Fragment() {
             logoImageView.setImageResource(R.drawable.logo)
         }
 
-//        //Coroutine
-//        GlobalScope.launch {
-//            delay(2000L)
-//            // Thi is where we will call the login page
+        //    Coroutine
+        GlobalScope.launch {
+            delay(2000L)
+//           // Thi is where we will call the login page
 //            // For now it is only popBackStack
-//            parentFragmentManager.popBackStack()
-//        }
 
+            parentFragmentManager.popBackStack()
+
+            parentFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(
+                    android.R.anim.slide_in_left,
+                    android.R.anim.slide_out_right,
+                    android.R.anim.slide_in_left,
+                    android.R.anim.slide_out_right,
+                )
+                .add(R.id.main_frameLayout, loginScreenFragment)
+                .addToBackStack(null)
+        }
 
     }
-
-
 }
+
+
+
+
+
